7a8ea7b6f1fcce8efcf845357d347e989956cedb
ridomin <ridomin@live.com>
2019-11-19 23:03:53 -0800

    Add project files.

---
+++ MyBaseConsoleApp/MyClass.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBaseConsoleApp
+{
+    public class MyClass
+    {
+        private readonly ILogger _logger;
+        public MyClass(ILogger<MyClass> logger)
+        {
+            _logger = logger;
+        }
+
+        public void DoWork()
+        {
+            _logger.LogWarning("Warn Start DoWork");
+
+        }
+
+        public async Task DoWorkAsync()
+        {
+            _logger.LogInformation("Start DoWorkAsync");
+            string[] lines =  await File.ReadAllLinesAsync("appsettings.json");
+            _logger.LogInformation("Stop DoWorkAsync");
+        }
+
+
+    }
+}
---
+++ MyBaseConsoleApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace MyBaseConsoleApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("starting");
+            ServiceProvider serviceProvider = ConfigureServices();
+
+            var myClass = serviceProvider.GetService<MyClass>();
+
+            myClass.DoWork();
+            await myClass.DoWorkAsync();
+            //the logger by adding a parameter (ILogger<MyClass>)
+            var logger = serviceProvider.GetService<ILogger<Program>>();
+
+            logger.LogInformation("Log in Program.cs");
+            logger.LogWarning("Warning");
+            logger.LogError("ERRROOORR");
+
+            Console.ReadLine();
+        }
+
+        private static ServiceProvider ConfigureServices()
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                                            .AddJsonFile("appsettings.json")
+                                            .Build();
+
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddLogging(configure => 
+                configure.AddConsole(ops => ops.TimestampFormat = "hh:mm:ss")
+            .AddConfiguration(configuration.GetSection("Logging")))
+            .AddTransient<MyClass>();
+
+            //LogLevel minLevel = Enum.Parse<LogLevel>(configuration["Logging:Console:LogLevel:Default"].ToString());
+            //serviceCollection.Configure<LoggerFilterOptions>(options => options.MinLevel = minLevel);
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+            return serviceProvider;
+        }
+
+    }
+}
